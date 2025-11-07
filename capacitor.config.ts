21bc5fac25ed091f3470aa6e3f8a8461fce2d035
Øystein Grande <oystgran@stud.ntnu.no>
2025-11-07 12:33:32 +0100

    Using Ionic/Capacitor instead of Android studio project files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'ShoppingList.tmp',
+  webDir: 'dist'
+};
+
+export default config;
