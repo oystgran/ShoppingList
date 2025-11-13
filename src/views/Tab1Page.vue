fdf0d74c551189f43694e0faf44722e7ff5e68a3
Øystein Grande <oystgran@stud.ntnu.no>
2025-11-13 17:31:17 +0100

    Possibility to write in text field, done/undone fields added

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -7,7 +7,40 @@
     </ion-header>
 
     <ion-content class="ion-padding">
-      <p>Start på handlelisten her...</p>
+      <!-- Input for nytt item -->
+      <ion-item>
+        <ion-input
+          v-model="newItem"
+          placeholder="Legg til vare…"
+          @keyup.enter="addItem"
+        ></ion-input>
+      </ion-item>
+
+      <!-- Ugjort -->
+      <ion-list>
+        <ion-list-header>Ukjøpt</ion-list-header>
+        <ion-item
+          v-for="item in undoneItems"
+          :key="item.id"
+          button
+          @click="toggle(item)"
+        >
+          {{ item.text }}
+        </ion-item>
+      </ion-list>
+
+      <!-- Ferdig -->
+      <ion-list>
+        <ion-list-header>Kjøpt</ion-list-header>
+        <ion-item
+          v-for="item in doneItems"
+          :key="item.id"
+          button
+          @click="toggle(item)"
+        >
+          <s>{{ item.text }}</s>
+        </ion-item>
+      </ion-list>
     </ion-content>
   </ion-page>
 </template>
@@ -20,4 +53,35 @@ import {
   IonTitle,
   IonContent,
 } from "@ionic/vue";
+
+import { ref, computed } from "vue";
+
+interface Item {
+  id: number;
+  text: string;
+  done: boolean;
+}
+
+const newItem = ref("");
+const items = ref<Item[]>([]);
+
+function addItem() {
+  const text = newItem.value.trim();
+  if (!text) return;
+
+  items.value.push({
+    id: Date.now(),
+    text,
+    done: false,
+  });
+
+  newItem.value = "";
+}
+
+function toggle(item: Item) {
+  item.done = !item.done;
+}
+
+const undoneItems = computed(() => items.value.filter((i) => !i.done));
+const doneItems = computed(() => items.value.filter((i) => i.done));
 </script>
