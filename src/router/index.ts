2516cc420dbe6e22ade06a7620c4e556e0424c67
Øystein Grande <oystgran@stud.ntnu.no>
2025-11-21 20:00:43 +0100

    Removed "tabspage", "tab1page" renamed to Listpage, routes modified

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,25 +1,11 @@
 import { createRouter, createWebHistory } from "@ionic/vue-router";
 import { RouteRecordRaw } from "vue-router";
-import TabsPage from "../views/TabsPage.vue";
+import ListPage from "../views/ListPage.vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
-    redirect: "/tabs/tab1",
-  },
-  {
-    path: "/tabs/",
-    component: TabsPage,
-    children: [
-      {
-        path: "",
-        redirect: "/tabs/tab1",
-      },
-      {
-        path: "tab1",
-        component: () => import("@/views/Tab1Page.vue"),
-      },
-    ],
+    component: ListPage,
   },
 ];
 
